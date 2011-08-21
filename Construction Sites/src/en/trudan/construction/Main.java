1e079268749be0c9c0be9134d268f1f420b84949
Marvin Hofmann <kostronor@googlemail.com>
2011-08-21 22:22:35 +0200

    adding raw Filehandler

--- Construction Sites/src/en/trudan/construction/Main.java
+++ Construction Sites/src/en/trudan/construction/Main.java
@@ -5,11 +5,14 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import en.trudan.construction.data.FileHandler;
+
 public class Main extends JavaPlugin implements Plugin {
 
 	Logger log = Logger.getLogger("Minecraft");
 	Runnable runnable = new ConstructionThread();
 	Thread thread = new Thread(runnable);
+	private static FileHandler FH = null;
 	
 	
 	@Override
@@ -22,10 +25,16 @@ public class Main extends JavaPlugin implements Plugin {
 	@Override
 	public void onEnable() {
 		log.info("[Construction Sites] Plugin is starting up");
+		log.info("[Construction Sites] Loading data");
+		FH = new FileHandler();
 		log.info("[Construction Sites] Starting new thread to reduce lag");
 		thread.start();
 		log.info("[Construction Sites] Plugin has been enabled");
 		
+	}
+	
+	public FileHandler getFH(){
+		return FH;
 	}
 
 }
