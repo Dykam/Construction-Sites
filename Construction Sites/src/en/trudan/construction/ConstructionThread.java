de6b8d91cd238bd04f555bf0151465a5df3211b9
Dan Spiteri <danielspiteri@live.co.uk>
2011-08-21 20:07:35 +0100

    Added some multithreading shizzle because i'm that cool

---
+++ Construction Sites/src/en/trudan/construction/ConstructionThread.java
@@ -0,0 +1,11 @@
+package en.trudan.construction;
+
+public class ConstructionThread implements Runnable {
+
+	@Override
+	public void run() {
+		// Insert Main shizzle here :)
+
+	}
+
+}
--- Construction Sites/src/en/trudan/construction/Main.java
+++ Construction Sites/src/en/trudan/construction/Main.java
@@ -8,6 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin implements Plugin {
 
 	Logger log = Logger.getLogger("Minecraft");
+	Runnable runnable = new ConstructionThread();
+	Thread thread = new Thread(runnable);
 	
 	
 	@Override
@@ -19,7 +21,9 @@ public class Main extends JavaPlugin implements Plugin {
 
 	@Override
 	public void onEnable() {
-		
+		log.info("[Construction Sites] Plugin is starting up");
+		log.info("[Construction Sites] Starting new thread to reduce lag");
+		thread.start();
 		log.info("[Construction Sites] Plugin has been enabled");
 		
 	}
