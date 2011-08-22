1d4c385dbfeae40c5c590f190a869a6fb09a8f96
Dan Spiteri <danielspiteri@live.co.uk>
2011-08-22 19:18:29 +0100

    Inventory Listener

---
+++ src/en/trudan/ConstructionSites/Listeners/CSInventoryListener.java
@@ -0,0 +1,14 @@
+package en.trudan.ConstructionSites.Listeners;
+
+import org.getspout.spoutapi.event.inventory.InventoryCloseEvent;
+import org.getspout.spoutapi.event.inventory.InventoryListener;
+
+public class CSInventoryListener extends InventoryListener {
+
+	public void onInventoryClose(InventoryCloseEvent event) {
+		
+		
+		
+	}
+	
+}
