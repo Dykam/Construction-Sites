fcd3c9cc6469deac62e7e3945b036ed6e6590eb0
Marvin Hofmann <kostronor@googlemail.com>
2011-08-22 11:26:06 +0200

    adding pricelist

--- src/en/trudan/construction/data/FileHandler.java
+++ src/en/trudan/construction/data/FileHandler.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
+import java.util.Properties;
 
 public class FileHandler {
 
@@ -13,9 +14,11 @@ public class FileHandler {
 	//global stuff
 	//ms = MaterialStorage
 	private static HashMap<String,MaterialStorage> ms = null;
+	private static Properties pl = null;
 	//Folder-variables
 	static String mainDirectory = "plugins/Construction Sites"; //sets the main directory for easy reference
 	static File Storage = new File(mainDirectory + File.separator + "Storage.dat");
+	static File Prices = new File(mainDirectory + File.separator + "Prices.prop");
 	//onEnable
 	public FileHandler(){
 
@@ -25,11 +28,20 @@ public class FileHandler {
 		}else{
 			ms = new HashMap<String,MaterialStorage>();
 		}	
+		if(Prices.exists()){
+			load();
+		}else{
+			pl = new Properties();
+		}
 	}
 	public void save(){
 		try{
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(Storage));
 			oos.writeObject(ms);
+			oos.flush();
+			oos.close();
+			oos = new ObjectOutputStream(new FileOutputStream(Prices));
+			pl.store(oos, "Pricelist");
 			oos.flush();
 			oos.close();
 			//Handle I/O exceptions
@@ -45,6 +57,8 @@ public class FileHandler {
 			Object result = ois.readObject();
 			//As long as it is not corrupted, we can just cast the object to hashmap
 			ms = (HashMap<String, MaterialStorage>)result;
+			ois = new ObjectInputStream(new FileInputStream(Prices));
+			pl.load(ois);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -61,5 +75,17 @@ public class FileHandler {
 		if(!ms.containsKey(name)){
 			ms.put(name, new MaterialStorage());
 		}
+	}
+	
+	public int getPrice(int blockID){
+
+		return Integer.parseInt(pl.getProperty("" + blockID, "0"));
+		
+	}
+	
+	public void setPrice(int blockID, int price){
+
+		pl.setProperty("" + blockID, "" + price);
+		
 	}
 }
